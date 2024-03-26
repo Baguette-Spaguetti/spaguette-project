29f92afc34a3c26e5021b5f5860cc5f395bec4e9
gianlucaascari <gianlu.ascari@gmail.com>
2024-03-26 11:15:26 +0200

    Setup of the push

--- App.js
+++ App.js
@@ -4,12 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-<<<<<<< HEAD
       <Text>Hello World !</Text>
-=======
-      <Text>Hello World! That's me</Text>
->>>>>>> ff08242241dfc66a75333325ed9d39cdd00c2c11
-      <StatusBar style="auto" />
     </View>
   );
 }
