29a5bb57c473b257a4c96ab3ee43a59fbd98c926
gianlucaascari <gianlu.ascari@gmail.com>
2024-04-05 09:25:31 +0200

    BAG-21: Updated mock data for categories

--- screens/recipe catalogue screens/CategoriesScreen.js
+++ screens/recipe catalogue screens/CategoriesScreen.js
@@ -10,22 +10,22 @@ const RecipesScreen = ({ navigation }) => {
     {
       id: 1,
       name: "Pasta",
-      cover: "https://images.unsplash.com/photo-1621996346565-e3dbc646d9a9?q=80&w=2706&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      cover: "https://www.giallozafferano.it/images/242-24236/Spaghetti-alle-vongole_360x300.jpg",
     },
     {
       id: 2,
       name: "Meat",
-      cover: "https://images.unsplash.com/photo-1529692236671-f1f6cf9683ba?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      cover: "https://www.giallozafferano.it/images/257-25714/Bistecca-alla-fiorentina_360x300.jpg",
     },
     {
       id: 3,
       name: "Fish",
-      cover: "https://images.unsplash.com/photo-1584300005420-38486f627b07?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      cover: "https://www.giallozafferano.it/images/266-26693/Pesce-spada-in-padella_360x300.jpg",
     },
     {
       id: 4,
       name: "Salad",
-      cover: "https://plus.unsplash.com/premium_photo-1673590981774-d9f534e0c617?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      cover: "https://www.giallozafferano.it/images/190-19037/Insalata-di-gamberi_360x300.jpg",
     }
   ])
 
